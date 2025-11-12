fe7d811ea551471f9725dd3cf9bc420463020f92
Алина <@DESKTOP-P40RFID>
2025-11-12 15:09:57 +0500

    Task3

---
+++ Tyuiu.RagozinaAD.Sprint5.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RagozinaAD.Sprint5.Task3.V6.Lib
+{
+    public class DataService : ISprint5Task3V6
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.bin" });
+            double y = Math.Round((double)x / (Math.Sqrt((double)x * (double)x + (double)x)), 3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint5.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Tyuiu.RagozinaAD.Sprint5.Task3.V6.Lib;
+namespace Tyuiu.RagozinaAD.Sprint5.Task3.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Check()
+        {
+            string path = @"C:\Usres\Алина\Desktop\soures\repos\Tyuiu.RagozinaAD.Sprint5\Tyuiu.RagozinaAD.Sprint5.Task3.V6\bin\Debug\net8.0\OutPutFileTask3.bin";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
+
