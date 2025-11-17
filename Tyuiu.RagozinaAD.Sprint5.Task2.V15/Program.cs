68f6ff3cd52f5a51f21b9a2cb4e72b3664aef833
Алина <@DESKTOP-P40RFID>
2025-11-17 20:42:37 +0500

    Task2

---
+++ Tyuiu.RagozinaAD.Sprint5.Task2.V15/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.RagozinaAD.Sprint5.Task2.V15.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] mtrx = new int[3, 3] { { 6, 1, 7 },
+                                  { 1, 8, 5 },
+                                  { 6, 6, 4 } };
+
+        int rows = mtrx.GetUpperBound(0) + 1;
+        int columns = mtrx.Length / rows;
+
+
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов, заполненный        *");
+        Console.WriteLine("* значениями с клавиатуры. Заменить нечетные элементы массива на 0.       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.SaveToFileTextData(mtrx);
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+
+    }
+}
