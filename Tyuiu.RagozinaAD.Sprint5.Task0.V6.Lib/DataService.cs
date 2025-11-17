a636cc65f7fe7440b873e7516746c23113e4c4ae
Алина <@DESKTOP-P40RFID>
2025-11-17 20:21:28 +0500

    Add project files.

---
+++ Tyuiu.RagozinaAD.Sprint5.Task0.V6.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RagozinaAD.Sprint5.Task0.V6.Lib
+{
+    public class DataService : ISprint5Task0V6
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt");
+            double y = Math.Round(((x / (Math.Sqrt(Math.Pow(x, 2) + x)))), 3);
+
+            File.WriteAllText(path, Convert.ToString(y));
+
+            return path;
+        }
+    }
+}
