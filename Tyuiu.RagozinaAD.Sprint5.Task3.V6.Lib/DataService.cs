e84408646bf2a22d61bc05901dcd688106ac75f9
Алина <@DESKTOP-P40RFID>
2025-11-17 20:58:08 +0500

    Task3

---
+++ Tyuiu.RagozinaAD.Sprint5.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RagozinaAD.Sprint5.Task3.V6.Lib
+{
+    public class DataService : ISprint5Task3V6
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            double y = Math.Round((double)x / (Math.Sqrt((double)x * (double)x + (double)x)), 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint5.Task3.V6/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.RagozinaAD.Sprint5.Task3.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = 3;
+
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить    *");
+        Console.WriteLine("* в текстовый файл OutPutFileTask3.bin и вывести на консоль.              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("x = " + x);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.SaveToFileTextData(x);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
