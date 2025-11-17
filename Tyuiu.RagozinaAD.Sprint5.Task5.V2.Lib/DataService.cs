002ec632d2d5f5caf937e327786acd2767d3a106
Алина <@DESKTOP-P40RFID>
2025-11-17 21:13:46 +0500

    Task5

---
+++ Tyuiu.RagozinaAD.Sprint5.Task5.V2.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RagozinaAD.Sprint5.Task5.V2.Lib
+{
+    public class DataService : ISprint5Task5V2
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path).Replace(".", ",");
+            double res = Math.Round((1 / (Math.Cos(Convert.ToDouble(strX)) + Convert.ToDouble(strX))) - 4.12 * Convert.ToDouble(strX), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint5.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Tyuiu.RagozinaAD.Sprint5.Task5.V2.Lib;
+namespace Tyuiu.RagozinaAD.Sprint5.Task5.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V1.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
