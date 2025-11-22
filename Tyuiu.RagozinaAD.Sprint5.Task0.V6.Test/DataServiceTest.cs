89f39e7eda0e51f353b7b62855f9ec71e494d11f
Алина <@DESKTOP-P40RFID>
2025-11-22 12:31:51 +0500

    Task0

--- Tyuiu.RagozinaAD.Sprint5.Task0.V6.Test/DataServiceTest.cs
+++ Tyuiu.RagozinaAD.Sprint5.Task0.V6.Test/DataServiceTest.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.RagozinaAD.Sprint5.Task0.V6.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Алина\Desktop\source\repos\Tyuiu.RagozinaAD.Sprint5\DataSprint5\InPutDataFileTask6V21.txt";
+            string path = @"C:\Users\Алина\Desktop\source\repos\Tyuiu.RagozinaAD.Sprint5\Tyuiu.RagozinaAD.Sprint5.Task0.V6\bin\Debug\net8.0\OutPutDataFileTask0V6.txt";
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
